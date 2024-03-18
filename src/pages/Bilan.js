a2328048183276b4e42a597abd85aaf8bb562e4e
unknown <jeanakoupaul@gmail.com>
2024-03-18 09:13:11 +0000

    retrait bilan

--- src/pages/Bilan.js
+++ src/pages/Bilan.js
@@ -30,7 +30,7 @@ export default function Bilan() {
          
     <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/")} > retour</button>
 
-    {(isLoader===true||bilan===null)?<RingLoader
+    {isLoader===true?<RingLoader
         color={"green"}
         size={60}
       />:
