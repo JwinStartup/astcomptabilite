d88c608f8290f886d8601c0a572377cedaa8ce01
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-18 07:09:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import InscriptionUser from "./pages/InscriptionUser.js";
 import ListeParents from "./pages/ListeParents.js";
 import ListePersonnel from "./pages/ListePersonnels.js";
 import PrivateRouter from "./context/PrivateRouter.js";
-import BilanById from "./page/bilanById.js"
+import BilanById from "./page/bilanByid.js"
 function App() {
   return (
     <div className="App">
