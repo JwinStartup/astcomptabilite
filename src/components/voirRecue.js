4b163306eb50b32767284429fe747f91f6ce3904
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-26 19:15:13 +0000

    Update voirRecue.js

--- src/components/voirRecue.js
+++ src/components/voirRecue.js
@@ -53,7 +53,7 @@ export default function VoirRecue({retour,value}) {
         Retour
         </button>
        
-        {instance.loading===true?<div className=''>Chargement...</div>
+        {instance.loading===true?<div className=''>Chargement...</div>:
           <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
          <a href={instance.url} download={`Reçue`}>
         Télecharger
