dc59d16e70f85e5c363129283bfd676c4d7cfe2f
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-27 07:50:16 +0000

    Update voirRecue.js

--- src/components/voirRecue.js
+++ src/components/voirRecue.js
@@ -70,7 +70,7 @@ export default function VoirRecue({retour,value}) {
         </button>
        
          
-                  <button type="button" className="text-white bg-red-700 hover:bg-red-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={() => download( )}>telecharger</button>  
+                  <button type="button" className="text-white bg-red-700 hover:bg-red-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={() => download()}>telecharger</button>  
              
        
           </div>
