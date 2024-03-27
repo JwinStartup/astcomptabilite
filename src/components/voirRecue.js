88767a8d5109cc86660b093dc5094d4b895eb6e6
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-27 13:07:49 +0000

    Update voirRecue.js

--- src/components/voirRecue.js
+++ src/components/voirRecue.js
@@ -44,7 +44,7 @@ const download=async()=>{
       "https://api.cloudinary.com/v1_1/cfcunadoc/image/upload",formdata
      ).then((response)=>{
       console.log(response.data)
-      dispatch(comptabiliteActions.partager(response.data))
+      dispatch(comptabiliteActions.partager({url:response.data.secure_url}))
       })
    retour()
 }
