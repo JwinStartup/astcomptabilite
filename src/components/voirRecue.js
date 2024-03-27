c5072287410b03eafe22a624700b854a7e86d39f
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-27 08:14:29 +0000

    Update voirRecue.js

--- src/components/voirRecue.js
+++ src/components/voirRecue.js
@@ -76,7 +76,7 @@ const download=async()=>{
         </button>
        
          
-                  <button type="button" className="text-white bg-red-700 hover:bg-red-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={() => download()}>telecharger</button>  
+                  <button type="button" className="text-white bg-red-700 hover:bg-bleu-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={() => download()}>telecharger</button>  
              
        
           </div>
