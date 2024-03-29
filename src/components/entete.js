a1f8ee03b15be046504c73eb3b69a1b548b2db39
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 13:11:27 +0000

    Update entete.js

--- src/components/entete.js
+++ src/components/entete.js
@@ -16,7 +16,6 @@ console.log('auth:',user.auth)
   return (
     <div className=' w-full flex justify-between'>
           <h1 className='text-2xl m-3 text-green-300'>ASTComptable</h1>
-           {user.auth===false&&<Link to='/login'>connectez-vous</Link>}
            {user.me!==null&&<div className='flex m-2 items-center gap-1 '>
             <div className='bg-gray-400 rounded-full w-14 h-14'/>
              <p className=''>{user.me.nom} </p>
@@ -24,6 +23,11 @@ console.log('auth:',user.auth)
              <p className='text-sm tracking-widest text-green-300 pb-0.5'>{user.me.role}</p>
       
       </div>}
+           {user.auth!==false?
+             <Link to='/login'>connectez-vous</Link>:
+             <button onClick={()=> dispatch(userActions.deconnecte().then(()=>logout())} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+             deconnecte
+             </button>}
         </div>
   )
 }
