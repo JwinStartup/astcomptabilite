7654fd54b55ee320ac1e8397ea2fe80c7fef8514
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 08:56:35 +0000

    Update authContext.js

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,31 +1,28 @@
-import react from 'react'
-export default react.createContext({
-    auth:false,
-    user:null
-})
+import react,{createContext} from 'react'
+export const UserContext =createContext({auth:false,user:null})
 
 export const UserProvider = ({ children }) => {
   // User is the name of the "data" that gets stored in context
-  const [me, setMe] = useState({ user:null, auth: true });
+  const [user, setUser] = useState({ me:null, auth: true });
 
   // Login updates the user data with a name parameter
   const login = (user) => {
     setUser((user) => ({
-      user: user,
+      me: user,
       auth: true,
     }));
   };
 
   // Logout updates the user data to default
   const logout = () => {
-    setUser((user) => ({
-      user: null,
+    setUser(() => ({
+      me: null,
       auth: false,
     }));
   };
 
   return (
-    <UserContext.Provider value={{ me, login, logout }}>
+    <UserContext.Provider value={{ user, login, logout }}>
       {children}
     </UserContext.Provider>
   );
