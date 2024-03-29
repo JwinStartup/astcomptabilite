058bf9b24533b8cf58bdd295079361ab482c7dbd
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 19:45:07 +0000

    Update entete.js

--- src/components/entete.js
+++ src/components/entete.js
@@ -23,9 +23,9 @@ console.log('auth:',user.auth)
              <p className='text-sm tracking-widest text-green-300 pb-0.5'>{user.me.role}</p>
       
       </div>}
-           {user.auth!==false?
+           {user.me==null?
              <Link to='/login'>connectez-vous</Link>:
-             <button onClick={()=> dispatch(userActions.deconnecte())} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+             <button onClick={()=> dispatch(userActions.deconnecte().then(()=>logout() ))} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
              deconnecte
              </button>}
         </div>
