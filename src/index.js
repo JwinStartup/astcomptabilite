04072f3bd356f462b746c4cdda01fb82b349e108
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 08:36:48 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {UserProvider} from './context/authContext'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -11,7 +12,10 @@ root.render(
   <React.StrictMode>
      <BrowserRouter>
      <Provider store={store}>
-     <App /></Provider>
+      <UserProvider>
+     <App />
+    </UserProvider>
+  </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
