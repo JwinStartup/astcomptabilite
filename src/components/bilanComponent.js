0d27e2fd2c83fdb19f986e2d0cea239d3dc729c5
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-30 18:02:07 +0000

    Update bilanComponent.js

--- src/components/bilanComponent.js
+++ src/components/bilanComponent.js
@@ -48,11 +48,11 @@ export default function BilanComponent({retour,id}) {
          </tr>
        </tfoot>
       </table>
- <div classeName='flex flex-col '>
+ <div classeName='flex flex-col mt-3 space-x-3 '>
    <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
    retour
    </button>
-     <button type="button" onClick={()=>cloturer()} class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  ml-4 ">
+     <button type="button" onClick={()=>cloturer()} class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
    Cloturer
    </button>
      </div>
