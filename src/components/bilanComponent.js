36b9ca4f59f0387161cd07983d7526ff8667a626
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-30 17:37:45 +0000

    Update bilanComponent.js

--- src/components/bilanComponent.js
+++ src/components/bilanComponent.js
@@ -48,7 +48,7 @@ export default function BilanComponent({retour,id}) {
          </tr>
        </tfoot>
       </table>
- <div classeName='flex flex-col space-y-3'>
+ <div classeName='flex flex-col gap-3'>
    <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
    retour
    </button>
