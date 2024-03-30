3f86639b4f19b84ab5685407f158ae91057ee373
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-30 08:47:44 +0000

    Update authContext.js

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,10 +1,11 @@
 import react,{createContext,useState} from 'react'
+import {userReducer} from '../reducer/user'
 export const UserContext =createContext({auth:false,user:null})
 
 export const UserProvider = ({ children }) => {
   // User is the name of the "data" that gets stored in context
   const [user, setUser] = useState({ me:null, auth: true });
-
+   console.log(userReducer)
   // Login updates the user data with a name parameter
   const login = (p) => {
     console.log(p)
