06d974449fef03865f6359070b0c6b7feda9b503
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-31 09:24:10 +0000

    Update bilanComponent.js

--- src/components/bilanComponent.js
+++ src/components/bilanComponent.js
@@ -22,14 +22,25 @@ export default function BilanComponent({retour,id}) {
        {(isLoader===true||bilan===null)?<RingLoader
         color={"green"}
         size={60}
-      />:<> <div  className='flex  flex-col justify-start w-full mb-3'>
-        <div className='text-lg font-bold  '>
-         Bilan financier
-     </div>
-         <span className='tracking-wide text-sm text-gray-300 font-medium'>Mois:{bilan.periode} </span>
-        
-     </div>
+      />:<> 
+            
+        <div  className='flex  flex-col justify-start w-full mb-3'>
+            
+              <div className='text-lg font-bold'>
+              Bilan financier
+              </div>
+         
+              <span className='tracking-wide text-sm text-gray-300 font-medium'>Mois:{bilan.periode} </span>
+              <span 
+                   className=` ${bilan.statut==='cloturé'?text-red-400:text-green-400}
+                              tracking-wide text-sm  font-medium mt-1
+                              `>
+                              {bilan.statut} 
+                               </span>
+         </div>
+            
       <table className='w-full'>
+            
        <tbody>
          <tr className=' odd:bg-gray-100  bg-white rounded-3xl h-14 m-2  items-center w-full  cursor-pointer'>
            <td className='font-medium text-base text-gray-500 text-start'>Recettes</td>
@@ -46,15 +57,20 @@ export default function BilanComponent({retour,id}) {
            <td className='font-medium text-base text-gray-500 text-center'>{bilan.resultat||resultat} </td>
          </tr>
        </tfoot>
+    
       </table>
+    
  <div classeName='flex flex-col mt-3 space-x-3 '>
+    
    <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
    retour
    </button>
-      {bilan.statut!=='cloturé'&&<button type="button" onClick={()=>cloturer()} class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
+   {bilan.statut!=='cloturé'&&<button type="button" onClick={()=>cloturer()} class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
    Cloturer
    </button>
+          
      </div>
+          
    </>}
       </div>
   )
