48786ae2d1871d8c4ba97343f7b08814bc2ac7a6
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-31 13:44:05 +0000

    Update clientspersonnels.js

--- src/pages/clientspersonnels.js
+++ src/pages/clientspersonnels.js
@@ -9,12 +9,14 @@ export default function ClientsPersonnels() {
         <Entete />
        <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/")} > retour</button>
       <div className='w-full flex justify-center items-center mt-20 gap-4'>
-
-        <Link to='/cp/ListeParent' className="w-52 h-52 tracking-tight  text-black font-semibold bg-slate-200 rounded-3xl text-center items-center flex justify-center shadow-lg cursor-pointer">
-          Parents & Eleves 
+           <Link to='/cp/ListeParent' className="w-52 h-52 tracking-tight  text-black font-semibold bg-slate-200 rounded-3xl text-center items-center flex justify-center shadow-lg cursor-pointer">
+          Parents 
         </Link>
         <Link to='/cp/ListePersonnel' className="w-52 h-52 tracking-tight  text-black font-semibold bg-red-100 rounded-3xl text-center items-center flex justify-center">
           Personnels
+        </Link>
+        <Link to='/cp/ListeEnfant' className="w-52 h-52 tracking-tight  text-black font-semibold bg-slate-200 rounded-3xl text-center items-center flex justify-center shadow-lg cursor-pointer">
+           Eleves 
         </Link>
       </div>
     </div>
