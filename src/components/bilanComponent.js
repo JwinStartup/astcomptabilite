c2ad8392bb7bd9abf71cdc5c64e70d7594df5c84
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-31 09:41:19 +0000

    Update bilanComponent.js

--- src/components/bilanComponent.js
+++ src/components/bilanComponent.js
@@ -62,14 +62,23 @@ export default function BilanComponent({retour,id}) {
     
  <div classeName='flex flex-col mt-3 space-x-3 '>
     
-   <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+   <button 
+      type="button"
+      onClick={()=>retour()} 
+      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
    retour
    </button>
-   {bilan.statut!=='cloturé'&&<button type="button" onClick={()=>cloturer()} class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
-   Cloturer
+
+   {bilan.statut!=='cloturé'&&
+       <button 
+       type="button" 
+       onClick={()=>cloturer()} 
+       className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+        >
+       Cloturer
    </button>
           
-     </div>
+    
           
    </>}
       </div>
