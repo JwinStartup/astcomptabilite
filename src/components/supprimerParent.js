5197506792c76729fe291ececf523e7488d6eacc
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-01 21:38:39 +0000

    Update supprimerParent.js

--- src/components/supprimerParent.js
+++ src/components/supprimerParent.js
@@ -20,7 +20,7 @@ const dispatch=useDispatch()
     </svg>
     </button>
     
-    <button type="button" onClick={()=>retour()} className="text-white bg-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button type="button" onClick={()=>suppriner()} className="text-white bg-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Supprimer
     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
