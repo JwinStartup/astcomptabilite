dd128b0870c7dadf05a14733ec14ccf5c9cfe888
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-01 19:27:32 +0000

    Update supprimerPersonnel.js

--- src/components/supprimerPersonnel.js
+++ src/components/supprimerPersonnel.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch,useSelector } from 'react-redux';
-import { userActions } from '../../reducer/user.js'
+import { userActions } from '../reducer/user.js'
 export default function supprimerPersonnel({retour,rub}) {
 const dispatch=useDispatch()
   const supprimer=()=>{
