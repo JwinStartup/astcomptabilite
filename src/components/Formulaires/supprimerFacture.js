969dc9986f07d4c23f1a65cadb621cbcf37cfc9e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-02 07:09:31 +0000

    Update supprimerFacture.js

--- src/components/Formulaires/supprimerFacture.js
+++ src/components/Formulaires/supprimerFacture.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { comptabiliteActions } from '../../reducer/comptabilite';
 export default function supprimerFacture({retour,value}) {
+   const dispatch=useDispatch()
    const supprimer=()=>{
     dispatch(comptabiliteActions.supprimerFacture(value._id)).then(()=>{
    retour()
@@ -20,7 +21,7 @@ export default function supprimerFacture({retour,value}) {
         </svg>
         </button>
         
-        <button type="button"  onClick={()=>supprimer()} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="button"  onClick={()=>supprimer()} className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Supprimer
         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
