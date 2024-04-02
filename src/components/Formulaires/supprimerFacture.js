244bf8d3b0991cec27936951e2a74fd284ea4d36
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-02 06:59:17 +0000

    Update supprimerFacture.js

--- src/components/Formulaires/supprimerFacture.js
+++ src/components/Formulaires/supprimerFacture.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { comptabiliteActions } from '../../reducer/comptabilite';
-export default function supprimerFacture({retour}) {
+export default function supprimerFacture({retour,value}) {
+   const supprimer=()=>{
+    dispatch(comptabiliteActions.supprimerFacture(rub.value._id)).then(()=>{
+   retour()
+   })
+  }
   return (
     <div className='w-[500px] space-y-4 h-[200px]  items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une facture</div>
@@ -15,7 +20,7 @@ export default function supprimerFacture({retour}) {
         </svg>
         </button>
         
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="button"  onClick={()=>supprimer()} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Supprimer
         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
