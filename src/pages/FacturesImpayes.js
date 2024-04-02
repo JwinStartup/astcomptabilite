e6cb86ef18ca51381d074f1984d4c5d93a656b1e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-02 06:55:21 +0000

    Update FacturesImpayes.js

--- src/pages/FacturesImpayes.js
+++ src/pages/FacturesImpayes.js
@@ -37,7 +37,7 @@ const SetComponent=({p,retour,value})=>{
       return(
         <div>
           <Backdrop/>
-        <SupprimerFacture retour={retour} />
+        <SupprimerFacture retour={retour} value={value} />
         </div>
       )
   
