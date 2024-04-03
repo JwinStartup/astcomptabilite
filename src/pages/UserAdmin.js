05f1b1bbeb74e690ae4fc9c2eff7b2ae312bb84b
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 15:52:25 +0000

    Update UserAdmin.js

--- src/pages/UserAdmin.js
+++ src/pages/UserAdmin.js
@@ -54,12 +54,6 @@ export default function FacturesImpayes() {
      <div className='  flex justify-between  space-x-2 mx-4'>
      <div className=' mb-3 p-0  tracking-tight text-[22px] text-black font-semibold '><button className=' bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ml-2' onClick={()=>navigate("/")} > retour</button>Utilisateur </div>
      <div className='  flex justify-between  space-x-2'>
-                  <input type="search" className='w-[500px] bg-slate-400 rounded-3xl  pl-1 placeholder-white' placeholder='Numero de facture' />
-                <select name="" id="">
-                    <option value="">Aujourd'hui</option>
-                    <option value="">Ce mois</option>
-                    <option value="">Tous les mois</option>
-                </select>
                 <button onClick={()=>setRub({nom:'CREER',bol:true})} className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center'>+ Creer un utiilisateur</button>
             </div>
             </div>
@@ -87,8 +81,8 @@ export default function FacturesImpayes() {
       <td className='font-medium text-base text-gray-500 text-center'>{value.cel}</td>
       <td className='font-medium text-base text-gray-500 text-center'>{value.role}</td>
         <td className= {`font-medium text-base text-center ${value.type==='impaye'?'text-red-500':'text-green-500'}  `}>{value.type}</td>
-         {value.type==='impaye'&& <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'MODIFIER',bol:true,value:value} )}>Modifier </td>}
-           {value.type==='impaye'&& <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'SUPPRIMER',bol:true,value:value} )}>Supprimer </td>}
+          <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'MODIFIER',bol:true,value:value} )}>Modifier </td>
+            <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'SUPPRIMER',bol:true,value:value} )}>Supprimer </td>
     </tr>)}
   </tbody>
 </table>
