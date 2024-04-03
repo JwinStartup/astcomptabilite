7b616afe99a762179b32ff216cd7a26ad11d29ff
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 10:16:32 +0000

    Update Charges.js

--- src/pages/Charges.js
+++ src/pages/Charges.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Entete from '../components/entete'
 import  {useNavigate} from "react-router-dom"
 import AjouterCharges from "../components/ajouterCharges.js"
-import ModifierCharges from "../components/modifierCharge.js"
+import ModifierCharge from "../components/modifierCharge.js"
 import SupprimerCharge from "../components/supprimerCharge.js"
 import Backdrop from '../components/backdrop.js'
 import { useDispatch, useSelector } from 'react-redux'
