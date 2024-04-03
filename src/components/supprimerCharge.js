4988df2fb8400e9083ec083ea2bc08c4ab8e5600
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 11:07:20 +0000

    Update supprimerCharge.js

--- src/components/supprimerCharge.js
+++ src/components/supprimerCharge.js
@@ -10,7 +10,7 @@ const dispatch=useDispatch()
   return (
     <div className='w-[500px] space-y-4 h-[200px]  items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
     <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une charge</div>
-    <p className='font-lg  tracking-tight text-[18px] text-center' >Souhaitez-vous supprimer la  charge  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N*{value._id}  </span> </p>
+    <p className='font-lg  tracking-tight text-[18px] text-center' >Souhaitez-vous supprimer la  charge  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N*{value._id.slice(value._id.length-6)}  </span> </p>
 
     <div className='flex flex-col items-center  w-full h-full'>
     <div className='flex flex-row space-x-6'>  <button onClick={()=>retour()} type="button" className="text-white bg-red-700    font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
