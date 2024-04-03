a1ea17f559a68f0965b9f92bf4a8ccb96da00db3
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 06:35:09 +0000

    Update modifierParent.js

--- src/pages/modifierParent.js
+++ src/pages/modifierParent.js
@@ -21,16 +21,13 @@ export default function ModifierParents() {
      navigate('/cp/ListeParent')
     })
   }
-  useEffect(() => { 
-    dispatch(userActions.voirParent(id)).then((d)=>console.log(d))
-  },[])
-  
+
   return (
     <div>
       <Entete />
       <div className='flex items-cennter  mx-5 w-[1150px]'>
          <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/cp/ListeParent")} > retour</button>
-                <p className=' mb-3 p-0 ml-5  tracking-tight text-2xl text-black font-bold'>Inscription parent</p>
+                <p className=' mb-3 p-0 ml-5  tracking-tight text-2xl text-black font-bold'>Modifier parent</p>
                
             </div>
            <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex justify-center h-full items-center flex-col space-y-4 p-9 ' >
@@ -54,10 +51,10 @@ export default function ModifierParents() {
     
        
        
-<button type='submit' className='outline-none flex flex-row items-center justify-center space-x-2  text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-lg  px-5 py-2.5 mr-2 mb-2'>
+<button type='submit' className='outline-none flex flex-row items-center justify-center space-x-2  text-white bg-red-700 hover:bg-red-800  font-medium rounded-lg text-lg  px-5 py-2.5 mr-2 mb-2'>
        
         
-         <span>   S'inscrire</span>
+         <span>Modifier</span>
         </button>
       </form>
 
