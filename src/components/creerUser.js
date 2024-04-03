a3f0e440e3b9896ea5896e522ef1d3da88af71b0
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 20:22:48 +0000

    Update creerUser.js

--- src/components/creerUser.js
+++ src/components/creerUser.js
@@ -20,7 +20,7 @@ export default function InscriptionUser({retour}) {
     })
   }
   return (
-         <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='bg-white border-gray-100 shadow-md rounded-3xl  z-10 absolute top-[150px] left-[400px] flex justify-center w-[550px] mt-6 items-center flex-col space-y-4 p-9 border border-gray-400 rounded-lg shadow-lg' >
+         <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='bg-white border-gray-100 shadow-md rounded-3xl  z-10 absolute top-[80px] left-[400px] flex justify-center w-[550px] mt-6 items-center flex-col space-y-4 p-9 border border-gray-400 rounded-lg shadow-lg' >
          
      <div className='flex flex-col '>
    <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Inscrivez un utilisateur</p>             
