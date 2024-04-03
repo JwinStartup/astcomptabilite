04c263a1ff1cd52c2901b173afb4f0cc14056eb0
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 14:46:41 +0000

    Update FacturesImpayes.js

--- src/pages/FacturesImpayes.js
+++ src/pages/FacturesImpayes.js
@@ -68,7 +68,6 @@ export default function FacturesImpayes() {
   return (
     <div>
          {rub.bol!==false&&<SetComponent p={rub.nom} retour={()=>setRub({bol:false,nom:''})} value={rub.value} />}
-        {console.log(factures)}
         <Entete />
      <div className='  flex justify-between  space-x-2 mx-4'>
      <div className=' mb-3 p-0  tracking-tight text-[22px] text-black font-semibold '><button className=' bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ml-2' onClick={()=>navigate("/factures")} > retour</button>Factures </div>
