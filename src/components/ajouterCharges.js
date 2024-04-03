5056330b4df15774fe8a51d235a640f6a57ca193
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 15:10:40 +0000

    Update ajouterCharges.js

--- src/components/ajouterCharges.js
+++ src/components/ajouterCharges.js
@@ -61,7 +61,7 @@ const {isLoader,personnels} = useSelector((state)=>{
        <div className='flex flex-row space-x-4'> <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Retour
         </button>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Creer
         </button>
         </div>
