67b15f242f39e1266c440947bb8807c8a61e4183
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-03 20:13:15 +0000

    Update modifierUser.js

--- src/components/modifierUser.js
+++ src/components/modifierUser.js
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { userActions } from '../reducer/user';
 import { useNavigate } from 'react-router-dom';
 export default function ModifierUser({retour,value}) {
+  console.log(value)
   const navigate=useNavigate()
   const { register, handleSubmit,
     // formState: { errors }
