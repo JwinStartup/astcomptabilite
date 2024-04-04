796e097dfbc6165ea7afd3f0d1e6a2a23383ea5c
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-04 18:08:38 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ export default function Login() {
     })
   }
   return (
-    <div className='w-full flex flex-col  items-center justify-center bg-[url('../assets/homeCarreaux.png')]'>
+    <div className='w-full flex flex-col  items-center justify-center bg-home'>
         <Entete />
          <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}  className='flex justify-center w-[550px] mt-6 items-center flex-col space-y-4 p-9 border border-gray-400 rounded-lg shadow-lg' >
          
