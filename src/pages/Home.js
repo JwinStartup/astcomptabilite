6f8d12f72331b55a0e09091e1ce784cce34d45f1
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-04 21:37:08 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,15 +12,15 @@ export default function Home() {
         <Entete />
       <div className='w-full flex flex-wrap  h-full  justify-center items-center mt-20 gap-4 '>
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <div className="flex">
-        <FaFileInvoice  size={22} color="green"/>
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Facture & Reçue </h5>
+    <div className="flex items-center">
+        <FaFileInvoice  size={40} color="green"/>
+        <h5 className="mb-2 text-2xl font-bold ml-1 tracking-tight text-gray-900 dark:text-white">Facture & Reçue </h5>
     </div>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex  "> <GiCheckMark size={18} color="green" />Créer une facture ou reçue</p>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex"><GiCheckMark size={18} color="green" />Modifier une facture ou reçue</p>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex"><GiCheckMark size={18} color="green" />Payer une facture</p>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex"><GiCheckMark size={18} color="green" />Telecharger une facture ou reçue</p>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex"><GiCheckMark size={18} color="green" />Suppprimer une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center text-lg  "> <GiCheckMark size={18} color="green" />Créer une facture ou reçue</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center text-lg"><GiCheckMark size={18} color="green" />Modifier une facture ou reçue</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center text-lg"><GiCheckMark size={18} color="green" />Payer une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center text-lg"><GiCheckMark size={18} color="green" />Telecharger une facture ou reçue</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center text-lg"><GiCheckMark size={18} color="green" />Suppprimer une facture</p>
     <Link to='/factures' className className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Commençer
         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
