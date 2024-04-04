bbbd98ab4c74c2ca43e8b207ff156ee4e05c6294
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-04 21:16:15 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,22 @@ export default function Home() {
       <div className='w-48'>
         <Entete />
       <div className='w-full flex flex-wrap  h-full  justify-center items-center mt-20 gap-4 '>
+      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Facture</h5>
+    </div>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Créer une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"Modifier une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"Payer une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"Telecharger une facture</p>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"Suppprimer une facture</p>
+    <Link to='/factures' className className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Commençer
+        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </Link>
+</div>
         <Link to='/cp' className="w-52 h-52 bg-red-100 tracking-tight  text-black font-semibold rounded-3xl text-center items-center flex justify-center">
           clients & personnels
         </Link>
