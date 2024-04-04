6c92537961b84b4483cba5d15ac708896f93c81c
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-04 10:17:59 +0000

    Update formulaireCreerFacture.js

--- src/components/Formulaires/formulaireCreerFacture.js
+++ src/components/Formulaires/formulaireCreerFacture.js
@@ -6,7 +6,8 @@ import { useForm } from 'react-hook-form';
 export default function FormulaireCreerFacture({retour}) {
   const { register, handleSubmit,
   } = useForm(
-);
+);}
+  const [montant,setMontant]=useState(0)
   const dispatch =useDispatch()
   useEffect(() => { 
     dispatch(userActions.listeParent())
@@ -26,8 +27,8 @@ export default function FormulaireCreerFacture({retour}) {
   const  onChangeParent=(e)=>
     {
      const p= parents.find((d)=>
-       d._id===e)
-      console.log(p)
+       d._id===e).montantCours
+      setMontant(p)
     }
   return (
     <div className='w-[500px] h-[300px] border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[150px] left-[400px]'>
@@ -35,10 +36,10 @@ export default function FormulaireCreerFacture({retour}) {
         <form   onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex flex-col items-center mt-5  space-y-3 w-full h-full' >
         <select {...register("client")}
               onChange={(e)=>onChangeParent(e.target.value)} className='outline-none w-[400px] border-b-2 py-1 text-lg'>
-             <option className='text-gray-400' value=" " >Choississez un parent </option>     
+             <option className='text-gray-200' value=" " >Choississez un parent </option>     
          {parents.map((val,index)=> <option className='' value={val._id} key={index}> {val.nom}  {val.prenoms}</option>)}
         </select>
-        <input {...register("montant")}  type="number"  className='w-[400px] h-10 border-b-2 py-1 text-lg pl-1 placeholder-gray-300' placeholder='Montant prestation' />
+        <input {...register("montant")} defaultValue={montant} type="number"  className='w-[400px] h-10 border-b-2 py-1 text-lg pl-1 placeholder-gray-300' placeholder='Montant prestation' />
         <select {...register("periodeAjouter")}  defaultValue='Janvier 2024' className='outline-none w-[400px] border-b-2 py-1 text-lg'>
             <option>Janvier 2024</option>
             <option>Fevrier 2024</option>
