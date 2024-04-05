26d0ffe8aaaea047bf6cfe3293fcfa10ae0c5407
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-05 15:26:27 +0000

    Update Factures.js

--- src/pages/Factures.js
+++ src/pages/Factures.js
@@ -7,11 +7,11 @@ export default function Factures() {
   return (
     <div>
     <Entete />
-    <div className='flex items-cennter justify-between mx-5 w-[1150px]'>
+    <div className='flex items-cennter justify-between mx-5 '>
     <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/")} > retour</button>
             </div>
-  <div className='w-full flex justify-center items-center mt-20 gap-4'>
-    <Link to='/factures/commissions' className="w-52 h-52 tracking-tight  text-black font-semibold bg-slate-200 rounded-3xl text-center items-center flex justify-center shadow-lg cursor-pointer">
+  <div className='w-full flex justify-center items-center mt-20 '>
+  {/* <Link to='/factures/commissions' className="w-52 h-52 tracking-tight  text-black font-semibold bg-slate-200 rounded-3xl text-center items-center flex justify-center shadow-lg cursor-pointer">
       Commissions
     </Link>
     <Link to='/factures/impayes' className="w-52 h-52 tracking-tight  text-black font-semibold bg-red-100 rounded-3xl text-center items-center flex justify-center">
@@ -19,8 +19,24 @@ export default function Factures() {
     </Link>
     <Link to='/factures/recues' className="w-52 h-52 tracking-tight  text-black font-semibold bg-red-100 rounded-3xl text-center items-center flex justify-center">
       Reçues
-    </Link>
+    </Link>*/}
+      <div className='flex flex-col '>
+        <div className='flex items-center justify-center mb-2'>
+       <p className='text-sm font-bold text-gray-400'>
+        Factures recentes
+      </p>
+       <Link to='/factures/impayes' className='text-xs font-bold text-red-400'>
+        Voir plus
+      </Link>
+      </div>
+         {/* les 3 premieres factures crées  ou modifiés*/}
+        <div>
+          
+          </div>
+       
+      </div>
     
-     </div></div>
+     </div>
+  </div>
   )
 }
