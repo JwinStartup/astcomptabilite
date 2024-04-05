13de5bb4deab36e37a1e6295b586b0857a1d197c
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-05 21:37:08 +0000

    Update factureCompo.js

--- src/components/factureCompo.js
+++ src/components/factureCompo.js
@@ -2,17 +2,17 @@ import React from "react"
 import { FaUserTie } from "react-icons/fa6"
 export default function FactureComponent({facture}){
   return(
-      <div className='border rounded-md w-[170px] px-2'>
+      <div className='border rounded-md  px-2'>
            <div className='flex items-center mx-2'> 
-        <div className='font-bold  tracking-tight text-[18px] text-black pl-1'>Facture</div>
-        <div className='font-medium  tracking-tight text-[11px] text-green-400 pl-1'>N°{facture._id.slice(facture._id.length-3)} </div>
+        <div className='font-bold  tracking-tight text-[18px] text-black '>Facture</div>
+        <div className='font-medium  tracking-tight text-[11px] text-green-400 '>N°{facture._id.slice(facture._id.length-3)} </div>
       </div> 
       <div className='flex flex-col w-full'>
         <div className='ml-2'>
-        <div className='text-md font-medium text-gray-500 flex flex-row'><FaUserTie size={20} color="gray" />{facture.client.nom} {facture.client.prenoms} </div>
+        <div className='text-sm font-medium text-gray-500 flex flex-row'><FaUserTie size={20} color="gray" />{facture.client.nom} {facture.client.prenoms} </div>
         </div>
         <div className='ml-2 flex flex-col justify-center'>
-            <div className='font-bold  tracking-wide text-md text-black '>{facture.montant} FCFA</div>
+            <div className='font-bold  tracking-wide text-sm text-black '>{facture.montant} FCFA</div>
             {facture.type==="paye"?<div className= {`text-green-400 font-medium text-start tracking-tight text-[11px] `} >Montant payé </div>:<div className= {`text-red-400 font-medium text-start tracking-tight text-[11px] `} >Montant impayé</div>}
         </div>
 
