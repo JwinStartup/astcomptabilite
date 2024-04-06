c563809362a507006e770fcc42ce8cb8343f266a
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-06 12:46:27 +0000

    Update modifierFacture.js

--- src/components/Formulaires/modifierFacture.js
+++ src/components/Formulaires/modifierFacture.js
@@ -27,7 +27,7 @@ export default function ModifierFacture({retour,value}) {
     return state.userReducer
    });
   return (
-    <div className='w-[500px] my-5 border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[150px] left-[400px]'>
+    <div className='w-[300px] my-5 border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Modifier la facture N°{value._id.slice(value._id.length-6)} de {value.client.nom}  {value.client.prenoms} </div>
         <form   onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex flex-col items-center mt-5  space-y-3 w-full h-full' >
         <input {...register("montant")}  type="number"  className='w-[400px] h-10 border-b-2 py-1 text-lg pl-1 placeholder-gray-300' placeholder='Montant prestation' />
@@ -45,10 +45,10 @@ export default function ModifierFacture({retour,value}) {
             <option>Novembre 2024</option>
             <option>Decembre 2024</option>
         </select>
-       <div className='flex flex-row space-x-4'> <button type="button" onClick={()=>retour()} class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+       <div className='flex flex-row space-x-4'> <button type="button" onClick={()=>retour()} className="border-r text-red-400 hover:text-red-800  font-medium text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Retour
         </button>
-        <button type='submit' class="text-white bg-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type='submit' className="text-blue-400  hover:text-blue-800  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Modifier
         </button>
         </div>
