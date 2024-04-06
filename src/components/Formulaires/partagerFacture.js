80bde191caf9d1d83131b05cc3b598eb5be25a2e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-06 12:59:49 +0000

    Update partagerFacture.js

--- src/components/Formulaires/partagerFacture.js
+++ src/components/Formulaires/partagerFacture.js
@@ -49,8 +49,8 @@ useEffect(()=>{
    partager()
 })
   return (
-    <div className='w-[400px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[125]'>
-        <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une facture</div>
+    <div className='w-[300px]  space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl z-10 absolute top-[125px] left-[125px]'>
+        <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Partager une facture</div>
         <p className='font-lg  tracking-tight text-[18px] text-center' > Voulez-vous partager cette facture  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N* {value._id.slice(value._id.length-3)} </span> </p>
 
         <div className='flex flex-col items-center  w-full h-full'>
@@ -61,7 +61,7 @@ useEffect(()=>{
         </button>
          
         <button 
-       type="button" onClick={()=>download()} className=" text-bleu-700    font-medium border-r text-sm px-5 py-2.5 text-center inline-flex items-center">
+       type="button" onClick={()=>download()} className=" text-blue-700    font-medium border-r text-sm px-5 py-2.5 text-center inline-flex items-center">
         Telecharger
         </button>
       {ficher!==null&&
