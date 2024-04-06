d1ab38e1e3e9fdd4baf8c05fc6cb8806485af993
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-06 08:48:32 +0000

    Update partagerFacture.js

--- src/components/Formulaires/partagerFacture.js
+++ src/components/Formulaires/partagerFacture.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { comptabiliteActions } from '../../reducer/comptabilite';
+ import {WhatsappShareButton} from "react-share";
+ import {WhatsappIcon} from "react-share";
 export default function PartagerFacture({retour,value}) {
+   const [ficher,setFicher]=useState(null)
    const dispatch=useDispatch()
-   
+   const MyDoc = ({value})=>(
+  <Document pageMode='fullScreen' title={`Facture N° ${value._id.slice(value._id.length-6)}`}>
+        <Page size="A7" style>
+          <PDFfacture  value={value}/>
+          </Page>
+        </Document>
+);
  const download=async()=>{
  const blob = await pdf(
         <MyDoc value={value} />
@@ -18,7 +27,7 @@ export default function PartagerFacture({retour,value}) {
 
 }
   
-   {/*   const partager=async()=>{
+      const partager=async()=>{
  const blob = await pdf(
         <MyDoc value={value} />
     ).toBlob();
@@ -29,12 +38,14 @@ export default function PartagerFacture({retour,value}) {
      Axios.post(
       "https://api.cloudinary.com/v1_1/cfcunadoc/image/upload",formdata
      ).then((response)=>{
-      console.log(response.data)
-      dispatch(comptabiliteActions.partager({url:response.data.secure_url,filename:`Facture${value._id.slice(value._id.length-6)}.pdf`}))
+      setFicher(response.data.secure_url)
       })
-} */}
+} 
+useEffect(()=>{
+   partager()
+})
   return (
-    <div className='w-[400px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[50% ] left-[50%]'>
+    <div className='w-[300px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[50% ] left-[50%]'>
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une facture</div>
         <p className='font-lg  tracking-tight text-[18px] text-center' > Voulez-vous partager cette facture  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N* {value._id.slice(value._id.length-3)} </span> </p>
 
@@ -49,10 +60,18 @@ export default function PartagerFacture({retour,value}) {
        type="button" onClick={()=>download()} className=" text-bleu-700    font-medium border-r text-sm px-5 py-2.5 text-center inline-flex items-center">
         Telecharger
         </button>
-         
-        <button type="button"   className=" green-blue-700 font-medium text-sm px-5 py-2.5 text-center inline-flex items-center">
-        Partager
-        </button>
+      {fichier!==null&&
+         <WhatsappShareButton 
+         url={ficher}
+            title={`Votre facture N° ${value._id.slice(value._id.length-6)} a étè par ASTRAINIG BUSINESS`}
+            >
+              <button type="button"   className=" green-blue-700 font-medium text-sm px-5 py-2.5 text-center inline-flex items-center">
+                     <WhatsappIcon logoFillColor='white' size={20} round={true}> 
+                     </WhatsappIcon> 
+                     Partager 
+              </button>
+           </WhatsappShareButton>
+         }
          </div>
         </div>
     </div>
