64d80738f63f51eb207ff61d0d2a8a3d923329b8
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-06 12:48:38 +0000

    Update partagerFacture.js

--- src/components/Formulaires/partagerFacture.js
+++ src/components/Formulaires/partagerFacture.js
@@ -49,7 +49,7 @@ useEffect(()=>{
    partager()
 })
   return (
-    <div className='w-[300px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[50% ] left-[50%]'>
+    <div className='w-[400px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[125]'>
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une facture</div>
         <p className='font-lg  tracking-tight text-[18px] text-center' > Voulez-vous partager cette facture  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N* {value._id.slice(value._id.length-3)} </span> </p>
 
@@ -69,8 +69,8 @@ useEffect(()=>{
          url={ficher}
             title={`Votre facture N° ${value._id.slice(value._id.length-6)} a étè par ASTRAINIG BUSINESS`}
             >
-              <button type="button"   className=" green-blue-700 font-medium text-sm px-5 py-2.5 text-center inline-flex items-center">
-                     <WhatsappIcon logoFillColor='white' size={20} round={true}> 
+              <button type="button"   className=" text-green-700 font-medium text-sm px-5 py-2.5 text-center inline-flex items-center">
+                     <WhatsappIcon logoFillColor='white' size={30} round={true}> 
                      </WhatsappIcon> 
                      Partager 
               </button>
