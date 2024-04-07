a4a13ef5e327a742833ad7f4710e4512cb3fca1e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-07 19:37:24 +0000

    Update clientspersonnels.js

--- src/pages/clientspersonnels.js
+++ src/pages/clientspersonnels.js
@@ -8,14 +8,14 @@ export default function ClientsPersonnels() {
     <div>
         <Entete />
          <div className='w-[400px] flex border-b mx-2 py-2 justify-center items-center mt-5'>
-          <IoIosArrowDropleftCircle size={30} color="red" onClick={()=>navigate('/')} />
+          <IoIosArrowDropleftCircle size={25} color="red" onClick={()=>navigate('/')} />
            <Link to='/cp/ListeParent' className="tracking-tight w-full text-black font-semibold   text-center items-center border-r px-4 flex justify-center  cursor-pointer">
           Parents 
         </Link>
         <Link to='/cp/ListePersonnel' className="tracking-tight w-full text-black font-semibold   text-center items-center border-r px-4 flex justify-center  cursor-pointer">
           Personnels
         </Link>
-        <Link to='/cp/ListeEnfant' className="tracking-tight w-full text-black font-semibold   text-center items-center border-r px-4 flex justify-center  cursor-pointer">
+        <Link to='/cp/ListeEnfant' className="tracking-tight w-full text-black font-semibold   text-center items-center px-4 flex justify-center  cursor-pointer">
            Eleves 
         </Link>
       </div>
