e0c81d85eaabab90a1882bce4ec5aade6deb052e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-07 04:30:11 +0000

    Update supprimerFacture.js

--- src/components/Formulaires/supprimerFacture.js
+++ src/components/Formulaires/supprimerFacture.js
@@ -9,23 +9,19 @@ export default function SupprimerFacture({retour,value}) {
    })
   }
   return (
-    <div className='w-[500px] space-y-4 h-[200px]  items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
+    <div className='w-[400px] space-y-4   items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[350px]'>
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Supprimer une facture</div>
         <p className='font-lg  tracking-tight text-[18px] text-center' >Souhaitez-vous supprimer la facture  <span className='font-lg  tracking-tight text-[18px] text-green-400 pl-1'>N* {value._id.slice(value._id.length-6)} </span> </p>
 
-        <div className='flex flex-col items-center  w-full h-full'>
-        <div className='flex flex-row space-x-6'>  <button onClick={()=>retour()} type="button" className="text-white bg-red-700 hover:bg-red-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
+        <div className='flex flex-col items-center pb-3  w-full h-full'>
+        <div className='flex flex-row space-x-6'>  <button onClick={()=>retour()} type="button" className="border-r text-red-400 hover:text-red-700   font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Retour
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
+       
         </button>
         
-        <button type="button"  onClick={()=>supprimer()} className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="button"  onClick={()=>supprimer()} className="border-r text-blue-400 hover:text-blue-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Supprimer
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
+       
         </button> </div>
         </div>
     </div>
