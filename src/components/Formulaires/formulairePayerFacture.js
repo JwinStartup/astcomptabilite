a9f03bbc9947c519ea490ee3faeb53a456302ce5
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-07 04:37:21 +0000

    Update formulairePayerFacture.js

--- src/components/Formulaires/formulairePayerFacture.js
+++ src/components/Formulaires/formulairePayerFacture.js
@@ -20,14 +20,13 @@ export default function FormulairePayerFacture({retour,value}) {
  }
   console.log(value)
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='w-[300px]  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
+    <form onSubmit={handleSubmit(onSubmit)} className='w-[400px]  border p-3 bg-white border-gray-100 shadow-md rounded-lg   z-10 absolute top-[125px] left-[350px]'>
       <div className='flex flex-row justify-between w-full'> 
         <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Payer une facture</div>
-        <div className='font-medium  tracking-tight text-[14px] text-green-400 pl-1'>N°  {value._id} </div>
+        <div className='font-medium  tracking-tight text-[14px] text-green-400 pl-1'>N°  {value._id.slice(value._id.length-3)} </div>
       </div> 
       <div className='flex flex-row w-full  justify-between my-4'>
         <div className='ml-7'>
-        <div className='text-sm font-medium text-gray-500'>id : {value.client._id}</div>
         <div className='text-lg font-medium text-gray-500'>{value.client.nom} {value.client.prenoms}</div>
         <div className='text-sm font-medium text-gray-500'>{value.client.cel}</div>
 
@@ -43,8 +42,8 @@ export default function FormulairePayerFacture({retour,value}) {
        
 
 
-      <div className='mx-5'>
-       <label for="bordered-Select" class="w-full py-4 ms-2   text-sm font-medium text-red-500">Mode de paiement :</label>
+      <div className='mx-5 w-full'>
+       <label  className="w-full py-4 ms-2   text-sm font-medium text-red-500">Mode de paiement :</label>
         <select {...register("mode")} onChange={(e)=>setSelect(e.target.value)}  defaultValue='espece' className='w-[200px] outline-none ml-3  border-b-2  text-lg'>
             <option value='espece'>Espece</option>
             <option value='orangemoney'>Orange money</option>
@@ -54,13 +53,13 @@ export default function FormulairePayerFacture({retour,value}) {
         </select>
         </div>
         <div className='mx-7 mt-3'>
-       {(select==='orangemoney'||select==='mmtnmoney'||select==='moovnmoney'||select==='wave')&&<input type="text" {...register("ref")}  className='w-[200px] h-10 border-b-2 mb-2 text-lg pl-1 placeholder-gray-300' placeholder='id transaction' />}
+       {(select==='orangemoney'||select==='mmtnmoney'||select==='moovnmoney'||select==='wave')&&<input type="text" {...register("ref")}  className='w-full h-10 border-b-2 mb-2 text-lg pl-1 placeholder-gray-300' placeholder='id transaction' />}
         </div>
         <div className='flex flex-col items-center my-2  space-y-3 w-full h-full'>
         <div className='flex flex-row space-x-6'>  <button onClick={()=>retour()} type="button" className="text-blue-400 border-r  hover:text-blue-700   font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Retour
         </button>
-        <button type="submit" className="text-white text-red-400 hover:text-red-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
+        <button type="submit" className="text-red-400 hover:text-red-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Payer
         </button> </div>
         </div>
