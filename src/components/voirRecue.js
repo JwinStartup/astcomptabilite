d9b2de7ab09683b73259e488f46ec3471d4b15b5
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-07 14:27:32 +0000

    Update voirRecue.js

--- src/components/voirRecue.js
+++ src/components/voirRecue.js
@@ -62,7 +62,7 @@ useEffect(()=>{
 },[])
 
   
- console.log(value,ficher)
+ console.log(value,recue)
   return ( <div>
  <div className='w-[300px]  border p-3 bg-white border-gray-100 shadow-md rounded-md   z-10 absolute top-[200px] left-[50px]'>
    <div className='flex flex-row justify-between w-full'> 
