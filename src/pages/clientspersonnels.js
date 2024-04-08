b636ff336b19af9be026b2cd8c16154d622eae88
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-08 23:16:36 +0000

    Update clientspersonnels.js

--- src/pages/clientspersonnels.js
+++ src/pages/clientspersonnels.js
@@ -5,7 +5,7 @@ import ParentListe from '../components/parentListe'
 import PersonnelListe from '../components/personnelListe'
 import EleveListe from '../components/eleveListe'
 import { IoIosArrowDropleftCircle } from "react-icons/io";
-
+import { FaPlusCircle } from "react-icons/fa"
 const SetComponent=({p})=>{
   console.log(p)
   switch (p) {
