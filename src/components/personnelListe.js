e6fbd9baee96fb624c6a337423507ea3ad2361ac
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-08 21:51:31 +0000

    Update personnelListe.js

--- src/components/personnelListe.js
+++ src/components/personnelListe.js
@@ -16,33 +16,39 @@ export default function PersonnelListe() {
   console.log(personnels)
   return(
      <div  className='w-full justify-center items-center '>
-    <div className='w-full justify-center items-center mt-10 '> 
+   
+     {isLoader?<div className='w-full justify-center items-center mt-10 '> 
     <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
     </svg>
-</div>
-
-<div className="flex px-2 w-full  items-center">
+</div>:
+  <div>
+     {personnels.map((i,j)=>
+        
+        <div className="flex px-2 w-full  items-center">
     <div> 
-      <Avatar name="AKOU JEAN PAUL" size="60" round={true} /> 
+      <Avatar name={`{i.nom} {i.prenoms} `}  size="60" round={true} /> 
     </div>
     
     
       <div className="flex flex-col p-2 ">
     
-       <div className="text-xl font-bold ">AKOU JEAN PAUL </div>
-       <div className="text-gray-300 text-md font-bold ">0908070602</div>
-       <div className="flex justify-start text-red-300 font-medium text-sm items-center gap-1">Abidjan<div className="w-2 h-2 rounded-full bg-black"/>Cocody </div>
+       <div className="text-xl font-bold ">{i.nom} {i.prenoms} </div>
+       <div className="text-gray-300 text-md font-bold ">{i.cel} </div>
+       <div className="flex justify-start text-red-300 font-medium text-sm items-center gap-1">{i.ville}<div className="w-2 h-2 rounded-full bg-black"/>{i.commune} </div>
    
     </div>
     
       <div className="flex ml-3 p-4 flex-col items-center justify-center">
-         <div className="text-black font-medium text-md">Anglais - Français</div>
+         <div className="text-black font-medium text-md">{i.discipline}</div>
          <div className="text-green-400 font-bold text-sm">personnel</div>
       </div>
     
     </div>
+                    )}
+     </div>
+}
     </div>
 
   )
