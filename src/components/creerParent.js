34f8ed4995c3f90e4c5501c2a194a8ad8f23c110
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 08:24:32 +0000

    Update creerParent.js

--- src/components/creerParent.js
+++ src/components/creerParent.js
@@ -1,5 +1,4 @@
 import React,{useEffect,useState,} from 'react'
-import {PDFfacture} from '../PDFfacture'
 import { useDispatch, useSelector } from 'react-redux'
 import  Axios  from 'axios';
  import {FadeLoader}from 'react-spinners'
