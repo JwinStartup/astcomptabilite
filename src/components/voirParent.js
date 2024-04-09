bd91eac8feabdaaa0b092933b09d54ac98fc9d6d
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 12:03:16 +0000

    Update voirParent.js

--- src/components/voirParent.js
+++ src/components/voirParent.js
@@ -41,7 +41,7 @@ const supprimer=()=>{
         <div className='flex flex-col items-center my-2  w-full'>
       <div className='flex flex-row '> 
         <button 
-       type="button" onClick={()=>modifier()} className=" text-green-700  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
+       type="button" onClick={()=>modifier()} className=" text-green-700 border-r  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
         Modifier
         </button>
         <button 
