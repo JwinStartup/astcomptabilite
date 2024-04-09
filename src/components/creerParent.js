5d78692f553c1eba8955bfe16587bce965a1bb4b
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 08:16:02 +0000

    Update creerParent.js

--- src/components/creerParent.js
+++ src/components/creerParent.js
@@ -1,7 +1,6 @@
 import React,{useEffect,useState,} from 'react'
 import {PDFfacture} from '../PDFfacture'
 import { useDispatch, useSelector } from 'react-redux'
-import { comptabiliteActions } from '../../reducer/comptabilite.js'
 import  Axios  from 'axios';
  import {FadeLoader}from 'react-spinners'
 import { AiFillCloseCircle } from "react-icons/ai";
