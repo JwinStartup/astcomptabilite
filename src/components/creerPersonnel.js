9cc50464f8ca5c2756b5f798d253ccc63a75ae8b
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 09:06:37 +0000

    Update creerPersonnel.js

--- src/components/creerPersonnel.js
+++ src/components/creerPersonnel.js
@@ -21,7 +21,31 @@ const supprimer=()=>{
 }
   return (
    <form  className='w-[300px]  border p-3 bg-white border-gray-100 shadow-md rounded-lg   z-10 absolute top-[200px] left-[50px]'>
-            
+
+       <div className='flex flex-col '>
+   <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Inscription Personnel</p>             
+      <div className='flex flex-col mx-4 space-y-4'>
+      <input {...register("nom")}   type='text' placeholder='Nom' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("prenoms")}   type='text' placeholder='Prenoms' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("cel")}   type='text' placeholder='Cel' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("email")}   type='email' placeholder='Email' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("whatshapp")}   type='text' placeholder='Whatshapp' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("ville")}   type='text' placeholder='ville' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("commune")}   type='text' placeholder='commune' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <select {...register("discipline")} defaultValue='Science' className='outline-none w-[250px] border-b-2 py-1 text-lg'>
+            <option>Science </option>
+            <option>Litterature</option>
+            <option>Mathematique</option>
+            <option>Physique chimie</option>
+            <option>Histoire Geographie</option>
+            <option>Anglais</option>
+            <option>Philosophie</option>
+        </select>
+     </div> 
+     </div>
+
+
+   
         <div className='flex flex-col items-center my-2  w-full'>
       <div className='flex flex-row '> 
         <button 
