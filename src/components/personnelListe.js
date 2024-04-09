6b96190524c7f8a65da2f8c659e68a64fd6c2c38
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 17:51:29 +0000

    Update personnelListe.js

--- src/components/personnelListe.js
+++ src/components/personnelListe.js
@@ -32,13 +32,6 @@ export default function PersonnelListe() {
     </div>
     
     
-      <div className="flex flex-col p-2 ">
-    
-       <div className="text-xl font-bold ">{i.nom} {i.prenoms} </div>
-       <div className="text-gray-300 text-md font-bold ">{i.cel} </div>
-       <div className="flex justify-start text-red-300 font-medium text-sm items-center gap-1">{i.ville}<div className="w-2 h-2 rounded-full bg-black"/>{i.commune} </div>
-   
-    </div>
     
      <div className="flex flex-col px-2 ">
     
