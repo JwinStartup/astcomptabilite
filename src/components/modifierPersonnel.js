f2c85e70efdc415631c7e33f88acd338f268f13d
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-09 07:56:18 +0000

    Create modifierPersonnel.js

---
+++ src/components/modifierPersonnel.js
@@ -0,0 +1,46 @@
+import React,{useEffect,useState,} from 'react'
+import {PDFfacture} from '../PDFfacture'
+import { useDispatch, useSelector } from 'react-redux'
+import { comptabiliteActions } from '../../reducer/comptabilite.js'
+import  Axios  from 'axios';
+ import {FadeLoader}from 'react-spinners'
+import { AiFillCloseCircle } from "react-icons/ai";
+
+
+import {useNavigate} from 'react-router-dom'
+
+
+
+
+
+export default function modifierPersonnnel({retour,value}) {
+const dispatch = useDispatch()
+const modifier=()=>{
+  console.log('modifier')
+}
+const supprimer=()=>{
+  console.log('supprimer')
+}
+  return (
+    <div className='relative mx-3 bg-slate-100 w-[300px]  border p-3  border-gray-100  rounded-md '>
+            
+        <div className='flex flex-col items-center my-2  w-full'>
+      <div className='flex flex-row '> 
+        <button 
+       type="button" onClick={()=>retour()} className=" text-blue-700 font-medium border-r text-sm px-3 py-2 text-center inline-flex items-center">
+        Retour
+        </button>
+        <button 
+       type="button" onClick={()=>Modifier()} className=" text-green-700  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
+        Modifier
+        </button>
+        <button 
+       type="button" onClick={()=>Supprime()} className=" text-green-700  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
+        Supprimer
+        </button>
+      
+         </div>
+        </div>
+    </div>
+  )
+}
