5e2b7265512aa8677146c5a1393e503f1993cb68
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 11:43:37 +0000

    Update profilPage.js

--- src/pages/profilPage.js
+++ src/pages/profilPage.js
@@ -17,37 +17,10 @@ const dispatch = useDispatch()
 
 
   return (
-    <div className='z-10 absolute top-[100px] left-[20px] mx-3 bg-slate-100 w-[300px]  border p-3  border-gray-100  rounded-md '>
-   <div className='absolute right-0 top-0 m-1 cursor-pointer  z-50 '>    
-            <AiFillCloseCircle color="gray" size={25} onClick={()=>retour()} />
-           </div> 
-      <div className='flex  flex-col items-center justify-center '>
-            <Avatar name={`${value.nom} ${value.prenoms}`}  size="100" round={true} /> 
-           <div className='flex  text-lg font-bold tracking-wider text-center w-full justify-center items-center '>
-            {value.nom}  {value.prenoms}
-          </div>
-           <div className='flex  text-md text-green-400   font-normal tracking-widest text-center w-full justify-center items-center '>
-            Parent 
-          </div>
-           <div className='flex  text-md font-bold tracking-wider text-center w-full justify-center items-center '>
-            {value.cel}  
-          </div>
-           <div className="flex justify-start text-red-300 font-medium text-sm items-center gap-1">{value.ville}<div className="w-2 h-2 rounded-full bg-black"/>{value.commune} </div>
-       </div>
-            
-        <div className='flex flex-col items-center my-2  w-full'>
-      <div className='flex flex-row '> 
-        <button 
-       type="button" onClick={()=>navigate(`/modifier/eleves/${value._id}`)} className=" text-green-700  border-r  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
-        Modifier
-        </button>
-        <button 
-       type="button" onClick={()=>supprimerEl(value)} className=" text-red-500  font-medium  text-sm px-3 py-2 text-center inline-flex items-center">
-        Supprimer
-        </button>
-      
-         </div>
-        </div>
+ <div className=' w-full'>
+    <div> 
+   
+   </div>
     </div>
   )
 }
