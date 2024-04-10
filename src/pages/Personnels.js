acd45f41382e564bf7c40f7ad830a15d6615cf90
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 07:46:49 +0000

    Update Personnels.js

--- src/pages/Personnels.js
+++ src/pages/Personnels.js
@@ -23,7 +23,7 @@ dispatch(userActions.inscriptionPersonnel(data)).then(()=>{
     <div>
     <Entete />
     <div className="flex  items-center "  onClick={()=>navigate('/cp')} >
-        <IoIosArrowDropleftCircle size={30} style={{paddingRigth:"5px"} color="black" />
+        <IoIosArrowDropleftCircle size={30} style={{paddingRigth:"5px"}} color="black" />
         <h5 className="text-xl font-bold ml-1 tracking-tight text-gray-900  ">Inscription personnel  </h5>
     </div>
          <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex justify-center h-full items-center flex-col space-y-4 p-9 ' >
