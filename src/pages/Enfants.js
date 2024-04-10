17022e9bef21e8111e0f7233f258021be27389e5
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 07:35:07 +0000

    Update Enfants.js

--- src/pages/Enfants.js
+++ src/pages/Enfants.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch,useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { userActions } from '../reducer/user';
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 export default function Enfants() {
  const [par,setPar] = useState([])
   const navigate=useNavigate()
