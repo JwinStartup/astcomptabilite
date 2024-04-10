5f6dfbf3573f6807ae4e940b7f96b8554c858f5c
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 09:38:33 +0000

    Update voirEnfant.js

--- src/components/voirEnfant.js
+++ src/components/voirEnfant.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function VoirEnfant({retour}) {
+export default function VoirEnfant({retour , sup}) {
   return (
     <div className='w-[500px] space-y-4 h-[200px]  items-center justify-center  border p-3 bg-white border-gray-100 shadow-md rounded-3xl   z-10 absolute top-[125px] left-[400px]'>
     <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Modifier Formateur</div>
@@ -17,9 +17,8 @@ export default function VoirEnfant({retour}) {
     
     </button>
     
-    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    Ajouter un Eleve
-   
+    <button type="button" onClick={()=>sup()} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    Supprimer un Eleve
     </button> </div>
     </div>
 </div>
