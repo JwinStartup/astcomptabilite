db1d2ee7a7e865906fefeab2ef6528df220a9983
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 06:52:31 +0000

    Update Enfants.js

--- src/pages/Enfants.js
+++ src/pages/Enfants.js
@@ -34,7 +34,7 @@ const {personnels} = useSelector((state)=>{
   return (
     <div>
     <Entete />
-    <div className='flex items-cennter justify-between mx-5 w-full'>
+    <div className='flex items-cennter justify-between mx-5 '>
     <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/cp/ListePersonnel")} > retour</button>
 
               <p className=' mb-3 p-0 ml-5  tracking-tight text-2xl text-black font-bold'>Inscription Ennfant</p>
