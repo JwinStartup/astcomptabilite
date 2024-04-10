f2a4fa91ef093f5558c1afa433fcde6330084124
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 07:38:27 +0000

    Update modifierEnfant.js

--- src/pages/modifierEnfant.js
+++ src/pages/modifierEnfant.js
@@ -41,7 +41,7 @@ const {personnels} = useSelector((state)=>{
     <Entete />
     <div className="flex  items-center "  onClick={()=>navigate('/cp')} >
         <IoIosArrowDropleftCircle size={30} style={{paddingRigth:"5px"}} color="black" />
-        <h5 className="text-2xl font-bold ml-1 tracking-tight text-gray-900  ">Inscription parent  </h5>
+        <h5 className="text-2xl font-bold ml-1 tracking-tight text-gray-900  ">Modifier élève  </h5>
     </div>
          <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex justify-center h-full items-center flex-col space-y- p-9 ' >
          
