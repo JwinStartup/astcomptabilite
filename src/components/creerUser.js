c222f218cb50e7829cd9265307113cbda26bed30
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 18:23:18 +0000

    Update creerUser.js

--- src/components/creerUser.js
+++ src/components/creerUser.js
@@ -20,34 +20,38 @@ export default function InscriptionUser({retour}) {
     })
   }
   return (
-         <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='bg-white border-gray-100 shadow-md rounded-3xl  z-10 absolute top-[80px] left-[400px] flex justify-center w-[550px] mt-6 items-center flex-col space-y-4 p-9 border border-gray-400 rounded-lg shadow-lg' >
-         
-     <div className='flex flex-col '>
-   <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Inscrivez un utilisateur</p>             
-      <div className='flex flex-col mx-4 space-y-4'>
-      <input {...register("nom")} type='text' placeholder='Nom utiilisateur' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("email")}  type='text' placeholder='Email' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("password")}  type='text' placeholder='Mot de passe' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("cel")}  type='text' placeholder='Cel ' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <select {...register("role")} defaultValue='Directeur' className='outline-none w-[400px] border-b-2 py-1 text-lg'>
+         <form  className='w-[300px] onSubmit={handleSubmit(onSubmit)}  border p-3 bg-white border-gray-100 shadow-md rounded-lg   z-10 absolute top-[70px] left-[50px]'>
+      <div className='absolute right-0 top-0 m-1 cursor-pointer  z-50 '>    
+            <AiFillCloseCircle color="gray" size={25} onClick={()=>retour()} />
+           </div> 
+      <div className='flex flex-col '>
+     <p className='text-lg font-semibold text-black tracking-wider mb-3'>Inscrivez un utilisateur</p>             
+        <div className='flex flex-col mx-4 space-y-2'>
+        <input {...register("nom")} type='text' placeholder='Nom utiilisateur' className='outline-nonew-[250px] border-b-2 py-1 text-sm'/>
+          <input {...register("email")}  type='text' placeholder='Email' className='outline-nonew-[250px] border-b-2 py-1 text-sm'/>
+          <input {...register("password")}  type='text' placeholder='Mot de passe' className='outline-nonew-[250px] border-b-2 py-1 text-sm'/>
+          <input {...register("cel")}  type='text' placeholder='Cel ' className='outline-nonew-[250px] border-b-2 py-1 text-sm'/>
+      <select {...register("role")} defaultValue='Directeur' className='outline-nonew-[250px] border-b-2 py-1 text-sm'>
             <option>Directeur</option>
             <option>Administrateur</option>
         </select>
-      <select {...register("zone")} defaultValue='Directeur' className='outline-none w-[400px] border-b-2 py-1 text-lg'>
+      <select {...register("zone")} defaultValue='Directeur' className='outline-nonew-[250px] border-b-2 py-1 text-sm'>
             <option>Cocody</option>
             <option>Bouake</option>
             <option>San pedro</option>
             <option>Yamoussoukro</option>
         </select>
-     </div> 
-     </div>
-     <div className='flex flex-row space-x-4'> <button type="button" onClick={()=>retour()} className="text-white bg-red-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
-        Retour
-        </button>
-        <button type="submit" class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
-        Creer
+       </div>
+       </div>
+   
+        <div className='flex flex-col items-center my-2  w-full'>
+        <button  
+         type="submit"  className="text-green-700  font-medium  text-md px-3 py-2 text-center inline-flex items-center">
         </button>
         </div>
     </form>
   )
 }
+
+
+
