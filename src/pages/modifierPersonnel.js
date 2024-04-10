0162227cdf0e3a3abcb9f780b9527f6a1555dae7
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 06:59:56 +0000

    Update modifierPersonnel.js

--- src/pages/modifierPersonnel.js
+++ src/pages/modifierPersonnel.js
@@ -8,38 +8,36 @@ export default function ModifierPersonnels() {
 const dispatch =useDispatch()
     const {id}=useParams()
   const navigate=useNavigate()
-  const { register, handleSubmit} = useForm(
+  const { register, handleSubmit,formState:{isSubmitting}} = useForm(
       { defaultValues: async () => dispatch(userActions.voirPersonnel(id)).then((d)=>{return d.payload })}
 );
 const onSubmit = (data) => {
  console.log(data)
  //setLoading(true)
 dispatch(userActions.modifierPersonnel(data)).then(()=>{
-  navigate('/cp/ListePersonnel')
+  navigate('/cp')
  })
 }
   return (
     <div>
     <Entete />
-    <div className='flex items-cennter justify-between mx-5 w-[1150px]'>
-    <button className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center' onClick={()=>navigate("/cp/ListePersonnel")} > retour</button>
-
-              <p className=' mb-3 p-0 ml-5  tracking-tight text-2xl text-black font-bold'>Modifier Personnel</p>
-             
-          </div>
+    <div className="flex  items-center "  onClick={()=>navigate('/cp')} >
+        <IoIosArrowDropleftCircle size={30} color="black" />
+        <FaFileInvoice  size={30} color="#1D4ED8" style={{paddingRigth:"5px"}}/>
+        <h5 className="text-xl font-bold ml-1 tracking-tight text-gray-900  ">Inscription personnel  </h5>
+    </div>
          <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex justify-center h-full items-center flex-col space-y-4 p-9 ' >
          
      <div className='flex flex-col '>
-   <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Formateur</p>             
-      <div className='flex flex-col mx-4 space-y-4'>
-      <input {...register("nom")}   type='text' placeholder='Nom' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("prenoms")}   type='text' placeholder='Prenoms' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("cel")}   type='text' placeholder='Cel' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("email")}   type='email' placeholder='Email' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("whatshapp")}   type='text' placeholder='Whatshapp' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("ville")}   type='text' placeholder='ville' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <input {...register("commune")}   type='text' placeholder='commune' className='outline-none w-[400px] border-b-2 py-1 text-lg'/>
-      <select {...register("discipline")} defaultValue='Science' className='outline-none w-[400px] border-b-2 py-1 text-lg'>
+      <div className='flex flex-col mx-4 space-y-2'>
+      <input {...register("nom")}   type='text' placeholder='Nom' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("prenoms")}   type='text' placeholder='Prenoms' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("cel")}   type='text' placeholder='Cel' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("email")}   type='email' placeholder='Email' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("whatshapp")}   type='text' placeholder='Whatshapp' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("ville")}   type='text' placeholder='ville' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <input {...register("commune")}   type='text' placeholder='commune' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <select {...register("discipline")} defaultValue='Science' className='outline-none w-[250px] border-b-2 py-1 text-lg'>
             <option>Science </option>
             <option>Litterature</option>
             <option>Mathematique</option>
@@ -51,14 +49,10 @@ dispatch(userActions.modifierPersonnel(data)).then(()=>{
      </div> 
      </div>
      
-<button type='submit' className='outline-none flex flex-row items-center justify-center space-x-2  text-white bg-red-700 hover:bg-red-800  font-medium rounded-lg text-lg  px-5 py-2.5 mr-2 mb-2'>
-       <span>   S'inscrire</span>
+<button type='submit' className='outline-none flex flex-row items-center justify-center  font-bold text-lg  px-5 py-2.5 mr-2 mb-2'>
+  {isSubmitting? <span>Inscrire</span>:<span>Inscrire...</span}
       </button>
     </form>
-
-
-
-
   </div>
   )
 }
