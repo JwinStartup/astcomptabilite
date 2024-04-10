ace40d615ceb126fddb416f74157931532863ad2
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 18:09:08 +0000

    Update UserAdmin.js

--- src/pages/UserAdmin.js
+++ src/pages/UserAdmin.js
@@ -57,40 +57,25 @@ export default function FacturesImpayes() {
                 <button onClick={()=>setRub({nom:'CREER',bol:true})} className='ml-10 bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center'>+ Creer un utiilisateur</button>
             </div>
             </div>
-            {isLoader===true?<RingLoader
-        color={"green"}
-        size={60}
-      />:
-  <> {users.length===0?<p className='text-center w-full'>Pas d'utilisateur </p>:
-            <div className='w-full flex justify-center mt-16 '>
-            <table className="w-full mx-2">
-  <thead>
-    <tr className="">
-      <th  className='border-b-2 text-gray-400'>Non </th>
-      <th  className='border-b-2 text-gray-400'>Email</th>
-      <th  className='border-b-2 text-gray-400'>Cel</th>
-      <th  className='border-b-2 text-gray-400'>Zone</th>
-     <th  className='border-b-2 text-gray-400'>Role</th>
-    </tr>
-  </thead>
-  <tbody>
-  {users.map((value,index)=><tr key={index} className=' odd:bg-gray-100  bg-white rounded-3xl h-14 m-2  items-center w-full hover:bg-green-100 cursor-pointer'>
-      <td className='font-medium text-base text-gray-500 text-center'>{value.nom} </td>      
-      <td className='font-medium text-base text-gray-500 text-center'>{value.email}</td>
-      <td className='font-medium text-base text-gray-500 text-center'>{value.cel}</td>
-      <td className='font-medium text-base text-gray-500 text-center'>{value.zone}</td>
-      <td className='font-medium text-base text-gray-500 text-center'>{value.role}</td>
-        <td className= {`font-medium text-base text-center ${value.type==='impaye'?'text-red-500':'text-green-500'}  `}>{value.type}</td>
-          <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'MODIFIER',bol:true,value:value} )}>Modifier </td>
-            <td className='font-medium text-base text-gray-500 text-center 'onClick={()=>setRub({nom:'SUPPRIMER',bol:true,value:value} )}>Supprimer </td>
-    </tr>)}
-  </tbody>
-</table>
-</div>
+            {isLoader?<div className='w-full flex justify-center items-center mt-10 '> 
+    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+</div>:
+  <div>
+     {users.map((i,j)=>
+        <div key={j} onClick={()=>voir(i)} className="flex px-2 py-5 w-full  items-center border-b hover:bg-gray-200">
+    <div> 
+      <Avatar name={`${i.nom} ${i.prenoms}`}  size="55" round={true} /> 
+    </div>
+      <div className="flex flex-col px-2 ">
+       <div className="text-md font-semibold ">{i.nom} {i.prenoms} </div>
+    </div>
+    </div>
+        )}
+     </div>
 }
-</>
-}
-    
     </div>
   )
 }
