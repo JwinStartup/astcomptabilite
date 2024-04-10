284f888d304a90a4de749654292761fd05612727
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-10 06:48:58 +0000

    Update Parents.js

--- src/pages/Parents.js
+++ src/pages/Parents.js
@@ -49,7 +49,7 @@ export default function Parents() {
       <button type='submit' className='outline-none flex flex-row items-center justify-center space-x-2  text-white bg-green-700 hover:bg-green-800  font-medium  text-md  px-5 py-2.5 mr-2 mb-2'>
        
         
-  {isSubmitting? <span>Inscrire</span>: <span>Inscrire.....</span>}
+  {isSubmitting? <span>Inscrire</span>: <span>Inscrire...</span>}
         </button>
       </form>
 
