fad1649ff57f928dbf3e562e0105bc0187c70236
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-11 13:22:20 +0000

    Create voirCharge.js

---
+++ src/components/voirCharge.js
@@ -0,0 +1,52 @@
+import React,{useEffect,useState,} from 'react'
+import {PDFfacture} from '../PDFfacture'
+import { useDispatch, useSelector } from 'react-redux'
+import { comptabiliteActions } from '../reducer/comptabilite.js'
+import  Axios  from 'axios';
+ import {FadeLoader}from 'react-spinners'
+import { AiFillCloseCircle } from "react-icons/ai";
+
+
+import {useNavigate} from 'react-router-dom'
+
+
+
+
+
+export default function VoirFacture({value,modifier,supprimer}) {
+const dispatch = useDispatch()
+
+  return (
+    <div className='relative mx-3 bg-slate-100 w-[300px]  border p-3  border-gray-100  rounded-md '>
+           <div className='absolute right-0 top-0 m-1 cursor-pointer '>    
+            <AiFillCloseCircle color="gray" size={25} onClick={()=>supprimer()} />
+           </div> 
+      <div className='flex flex-row justify-between w-full'> 
+        <div className='font-bold  tracking-tight text-[22px] text-black pl-1'>Charge</div>
+      </div> 
+      <div className='flex flex-row w-full  justify-between my-2'>
+        <div className='ml-2'>
+        <div className='text-sm font-bold text-gray-500'>type: {value.type}</div>
+        <div className='text-sm font-medium text-gray-500'>montant:{value.montant}</div>
+        </div>
+        <div>
+             <div className='font-bold  tracking-wide text-sm text-black '>{value.personnel.nom} {value.personnel.prenoms} FCFA</div>
+            <div className='font-medium text-center tracking-tight text-[11px] text-gray-400 '>personnel</div>
+        </div>
+
+      </div>
+            <div className='mx-4 font-medium  tracking-tight text-sm text-black '>{value.observation}</div>
+            <div className='font-medium mt-2 tracking-tight text-sm text-black w-full text-end'>{value.periodeAjouter}</div>
+        <div className='  mt-2'>
+        </div>
+        <div className='flex flex-col items-center my-2  w-full'>
+    <div className='flex flex-row '> 
+        <button 
+       type="button" onClick={()=>navigate('/')} className=" text-blue-700 font-medium border-r text-sm px-3 py-2 text-center inline-flex items-center">
+        Modifier
+        </button>
+         </div>
+        </div>
+    </div>
+  )
+}
