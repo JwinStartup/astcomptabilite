4c36cf8210e2e530230275f189211c1b590abc5f
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-12 19:51:49 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
 })
 .required()
   const { register, handleSubmit,
-   formState: { isSubmitted,isSubmitSuccessful,isSubmitting,isLoading,isValidating}
+   formState: { isSubmitted,isSubmitSuccessful,isSubmitting,isLoading,isValidating,formState: { errors }}
      } = useForm(
    { resolver: yupResolver(schema)}
    );
@@ -45,14 +45,16 @@ export default function Login() {
    <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Connectez-vous</p>             
       <div className='flex flex-col mx-1 space-y-4'>
       <input {...register("nom")}  type='text' placeholder='Nom utilisateur' className='outline-none w-full border-b-2 py-1 text-lg'/>
-      <input {...register("password")} type='text' placeholder='Mot de passe' className='outline-none w-full border-b-2 py-1 text-lg'/>
-     </div> 
+    {error.nom&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{error.nom}</p> }
+    <input {...register("password")} type='text' placeholder='Mot de passe' className='outline-none w-full border-b-2 py-1 text-lg'/>
+      {error.password&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{error.password}</p> }
+    </div> 
      </div>
     <div className='flex flex-row '>
       <button onClick={()=>navigate('/')} className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-800 mx-2'>
        <span>retour</span>
       </button>
-  <button type='submit' className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-800 mx-2'>
+  <button type='submit' disable={isSubmitting} className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-800 mx-2'>
      {isSubmitting===false?  <span>Se connecter</span>
       :<span>
        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
