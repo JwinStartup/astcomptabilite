1799f31d80ea6b68aaca71a5907543d4f3d6641e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-12 20:07:33 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -46,9 +46,9 @@ export default function Login() {
    <p className='text-2xl font-semibold text-black tracking-wider mb-3'>Connectez-vous</p>             
       <div className='flex flex-col mx-1 space-y-4'>
       <input {...register("nom")}  type='text' placeholder='Nom utilisateur' className='outline-none w-full border-b-2 py-1 text-lg'/>
-    {error.nom&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{error.nom}</p> }
+    {errors.nom&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{errors.nom}</p> }
     <input {...register("password")} type='text' placeholder='Mot de passe' className='outline-none w-full border-b-2 py-1 text-lg'/>
-      {error.password&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{error.password}</p> }
+      {errors.password&&<p className='text-sm font-semibold text-red-500 tracking-wider my-2'>{errors.password}</p> }
     </div> 
      </div>
     <div className='flex flex-row '>
