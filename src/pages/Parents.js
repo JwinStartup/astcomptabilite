94184ea85937531108481ad7da9ac7c319876139
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 23:25:34 +0000

    Update Parents.js

--- src/pages/Parents.js
+++ src/pages/Parents.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Entete from '../components/entete'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
