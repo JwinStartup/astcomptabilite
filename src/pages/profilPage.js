fe11be9020cbf00ffb5ba28974c18fac3fa26211
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 15:17:02 +0000

    Update profilPage.js

--- src/pages/profilPage.js
+++ src/pages/profilPage.js
@@ -43,7 +43,7 @@ const [rub , setRub]=useState({retour:false,id:null})
             {user.me.nom}  {user.me.prenoms}
           </div>
            <div className="flex justify-start text-red-300 font-medium text-sm items-center gap-1">{user.me.role} </div>
-            <div onClick={()=>dispatch(userActions.deconnecte()).then(()=>logout()).then(()=>navigate('/'))} className='text-md font-semibold tracking-wider py-3  px-1' >Se deconnecter</div>
+            <div onClick={()=>dispatch(userActions.deconnecte()).then(()=>userActions.logout())} className='text-md font-semibold tracking-wider py-3  px-1' >Se deconnecter</div>
            </div>
 
             <div className='mt-6'>
