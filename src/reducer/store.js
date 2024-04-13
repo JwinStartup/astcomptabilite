6d59e505253cad15d7544fbeb303bb5178e8a0a1
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 13:49:16 +0000

    Update store.js

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import  eventSlice  from "./event.js";
-// import eventReducer from "./event.js";
+ import userReducer from "./user.js";
 import reducer from "./combine.js";
 export const store = configureStore({
   reducer: reducer,
