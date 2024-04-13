761137d675885d3d9aecbe473e97a9f2304fed2c
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 21:38:59 +0000

    Update PrivateRouter.js

--- src/context/PrivateRouter.js
+++ src/context/PrivateRouter.js
@@ -1,4 +1,4 @@
-import React,{useContext,useState} from 'react'
+import React,{useContext,useState,useEffect} from 'react'
 import {Navigate,Outlet} from 'react-router-dom'
 import { history } from '../helper/helper'
 import { useSelector } from 'react-redux';
