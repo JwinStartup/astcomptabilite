2bd62654e698f05a486f31aa006c7840c7c1c351
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 13:24:57 +0000

    Update PrivateRouter.js

--- src/context/PrivateRouter.js
+++ src/context/PrivateRouter.js
@@ -1,12 +1,18 @@
 import React,{useContext,useState} from 'react'
 import {Navigate,Outlet} from 'react-router-dom'
 import {UserContext} from './authContext'
-
+import { history } from './helper/helper'
 const PrivateRouter =()=>{
 const { login,logout, user } = useContext(UserContext);
 const  [useur,setUseur] =useState(user)
 return(
-  useur.me!==null?<Outlet/>:<Navigate to="/login"  />
+ // useur.me!==null?<Outlet/>:<Navigate to="/login"  />
+   if (useur.me===null) {
+        // not logged in so redirect to login page with the return url
+        return <Navigate to="/login" state={{ from: history.location }} />
+    }
+
+    return children;
 )
 }
 export default PrivateRouter
