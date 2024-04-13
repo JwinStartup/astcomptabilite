fe51f7da9319ecff5ee50665384434db4c7e34d7
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 17:30:53 +0000

    Update PrivateRouter.js

--- src/context/PrivateRouter.js
+++ src/context/PrivateRouter.js
@@ -5,14 +5,14 @@ import { history } from '../helper/helper'
 import { useSelector } from 'react-redux';
 
 const PrivateRouter =({children})=>{
- const { login,logout, user } = useContext(UserContext);
+// const { login,logout, user } = useContext(UserContext);
    const {auth} = useSelector((state)=>{
     return state.userReducer
    });
  console.log(auth)
 const  [useur,setUseur] =useState(user)
  // useur.me!==null?<Outlet/>:<Navigate to="/login"  />
-   if (auth===null) {
+   if (!auth) {
         // not logged in so redirect to login page with the return url
         return <Navigate to="/login" state={{ from: history.location }} />
     }
