2c3f0ba1290c07f6569bc61047680181479d5d89
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 14:07:39 +0000

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -32,8 +32,6 @@ export default function Login() {
    dispatch(userActions.login(data)).then((d)=>{
      setChargement(false)
      console.log(d)
-    // login(d.payload.user)
-     //navigate('/')
      
     })
   }
