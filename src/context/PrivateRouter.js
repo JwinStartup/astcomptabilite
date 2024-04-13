22845b853cd8f9a9f544fff1021bac524b082315
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-13 14:26:28 +0000

    Update PrivateRouter.js

--- src/context/PrivateRouter.js
+++ src/context/PrivateRouter.js
@@ -2,8 +2,10 @@ import React,{useContext,useState} from 'react'
 import {Navigate,Outlet} from 'react-router-dom'
 import {UserContext} from './authContext'
 import { history } from '../helper/helper'
+import { useSelector } from 'react-redux';
+
 const PrivateRouter =({children})=>{
-const { login,logout, user } = useContext(UserContext);
+// const { login,logout, user } = useContext(UserContext);
 const  [useur,setUseur] =useState(user)
  // useur.me!==null?<Outlet/>:<Navigate to="/login"  />
    if (useur.me===null) {
