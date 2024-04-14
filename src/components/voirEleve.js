633eecd66c096a65df472df1e228624614220d47
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-14 09:44:38 +0000

    Update voirEleve.js

--- src/components/voirEleve.js
+++ src/components/voirEleve.js
@@ -19,7 +19,7 @@ const modifier=()=>{
 }
 
   return (
-    <div className='z-10 absolute top-[100px] left-[20px] mx-3 bg-slate-100 w-[300px]  border p-3  border-gray-100  rounded-md '>
+    <div className='z-10 absolute top-[100px] left-[30px] mx-3 bg-slate-100 w-[300px]  border p-3  border-gray-100  rounded-md '>
    <div className='absolute right-0 top-0 m-1 cursor-pointer  z-50 '>    
             <AiFillCloseCircle color="gray" size={25} onClick={()=>retour()} />
            </div> 
