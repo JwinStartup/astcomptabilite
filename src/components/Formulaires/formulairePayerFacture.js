ced0ac2e21dbf2664bc7fac29a0b47dfc70448c8
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-14 06:53:18 +0000

    Update formulairePayerFacture.js

--- src/components/Formulaires/formulairePayerFacture.js
+++ src/components/Formulaires/formulairePayerFacture.js
@@ -59,14 +59,7 @@ export default function FormulairePayerFacture({retour,value}) {
         <div className='flex flex-row space-x-6'>  <button onClick={()=>retour()} type="button" className="text-blue-400 border-r  hover:text-blue-700   font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
         Retour
         </button>
-        <button type="submit" className="text-red-400 hover:text-red-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center ">
-        Payer
-        </button> </div>
-        </div>
-    </form>
-  )
-}
-{chargement==false?<button type='submit' className='text-red-400 hover:text-red-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center '>
+        {chargement==false?<button type='submit' className='text-red-400 hover:text-red-700  font-medium  text-sm px-5 py-2.5 text-center inline-flex items-center '>
        <span>Payer</span>
       
       </button>:<span className='flex flex-row items-center space-x-2 justify-center inline-flex items-center px-3 py-2 text-md font-medium text-center text-red-400'>
@@ -75,4 +68,9 @@ export default function FormulairePayerFacture({retour,value}) {
                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                  </svg>
               Loading...
-           </span>}
+           </span>} </div>
+        </div>
+    </form>
+  )
+}
+
