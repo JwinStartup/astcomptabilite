04983a3bddfd7b12c76a52b1fdf8efd693232f71
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-18 09:47:59 +0000

    Update FacturesImpayes.js

--- src/pages/FacturesImpayes.js
+++ src/pages/FacturesImpayes.js
@@ -92,13 +92,13 @@ export default function FacturesImpayes() {
         <FaFileInvoice  size={30} color="#1D4ED8" style={{paddingRigth:"5px"}}/>
         <h5 className="text-2xl font-bold ml-1 tracking-tight text-gray-900  ">Facture  </h5>
     </div>
-          <div classNamme='flex items-center flex-col justify-center '>
+  {/* <div classNamme='flex items-center flex-col justify-center '>
                 <select className='text-sm font-medium'>
                     <option value="">Aujourd'hui</option>
                     <option value="">Ce mois</option>
                     <option value="">Tous les mois</option>
                 </select>
-             </div>
+             </div> */}
                  <FaPlusCircle color="gray" size={25}  onClick={()=>setRub({nom:'CREER',bol:true})}/>
             </div>
   
