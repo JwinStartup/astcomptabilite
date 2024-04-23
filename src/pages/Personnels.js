27ca6b3d4d3089ca5531495de76f7b133829f550
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-04-23 22:13:57 +0000

    Update Personnels.js

--- src/pages/Personnels.js
+++ src/pages/Personnels.js
@@ -34,6 +34,10 @@ dispatch(userActions.inscriptionPersonnel(data)).then(()=>{
       <div className='flex flex-col mx-4 space-y-2'>
       <input {...register("nom")}   type='text' placeholder='Nom' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
       <input {...register("prenoms")}   type='text' placeholder='Prenoms' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
+      <select {...register("sexe")} defaultValue='Homme' className='outline-none w-[250px] border-b-2 py-1 text-lg'>
+            <option>Homme</option>
+            <option>Femme</option>
+        </select>
       <input {...register("cel")}   type='text' placeholder='Cel' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
       <input {...register("email")}   type='email' placeholder='Email' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
       <input {...register("whatshapp")}   type='text' placeholder='Whatshapp' className='outline-none w-[250px] border-b-2 py-1 text-lg'/>
