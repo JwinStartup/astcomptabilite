6de2743c88d96b9da66006f2fc9618cb954e9ea3
unknown <jeanakoupaul@gmail.com>
2025-06-19 05:12:14 +0000

    mise a jour6

--- src/pages/FacturesImpayes.js
+++ src/pages/FacturesImpayes.js
@@ -117,7 +117,7 @@ export default function FacturesImpayes() {
       {rub.bol!==false&&<SetComponent p={rub.nom} retour={()=>setRub({bol:false,nom:''})} value={rub.value} />}
       <Entete />
       <div className='w-full flex flex-col space-y-4'>
-        <div className='flex flex-col justify-between items-center space-x-2 mx-4'>
+        <div className='flex flex-row justify-between items-center space-x-2 mx-4'>
           <div className="flex items-center" onClick={()=>navigate('/')} >
             <IoIosArrowDropleftCircle size={30} color="black" />
             <FaFileInvoice size={30} color="#1D4ED8" className="mx-2"/>
