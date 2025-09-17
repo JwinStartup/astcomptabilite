ca5729851da060c090845f74b22622613585e3e5
unknown <jeanakoupaul@gmail.com>
2025-09-17 18:41:09 +0000

    mise a jour des ok ok ok

--- src/pages/FormCoursDomicile.js
+++ src/pages/FormCoursDomicile.js
@@ -269,7 +269,7 @@ export default function FormCoursDomicile() {
             <button
               type="button"
               className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded-lg"
-              onClick={() => navigate(-1)}
+              onClick={() => navigate('/cd')}
             >
               Retour
             </button>
